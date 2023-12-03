cb1662f738f627ab0fad690a1c331d752b24c725
amine <devprogamine@gmail.com>
2023-12-03 21:15:19 +0100

    ajoute event mouse JS

---
+++ JAVASCRIPT/main.js
@@ -0,0 +1,12 @@
+window.addEventListener("mousemove", (e) => {
+    cursor.style.top = e.pageY + "px";
+    cursor.style.left = e.pageX + "px";
+  
+    cercle2.style.top = e.pageY + "px";
+    cercle2.style.left = e.pageX + "px";
+  
+    cercle3.style.top = e.pageY + "px";
+    cercle3.style.left = e.pageX + "px";
+  });
+
+  
